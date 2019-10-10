bc0d6204e7b730a98e7d8ed6d6fdf398c0e25fd4
june4432 <june4432@icloud.com>
2019-10-10 22:54:06 +0900

    next

---
+++ JuneWeather/Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  JuneWeather
+//
+//  Created by 이영준 on 10/10/2019.
+//  Copyright © 2019 이영준. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "4f3f4118-08e1-4193-a892-b0d642821a2e";
